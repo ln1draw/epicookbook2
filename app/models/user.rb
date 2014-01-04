0ffb3a086bd79927551c3b89f2709d9f9c2cf901
shanghaiellen@gmail.com <shanghaiellen@gmail.com>
2014-01-04 14:13:51 -0800

    fixes user error HELLZ YEAH

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  has_secure_password
   validates_presence_of :password, :on => :create
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
